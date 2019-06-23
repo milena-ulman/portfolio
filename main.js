57e05107d8a1e401e1913ead32138bce78fdb38a
Milena Ulman <milenadev94@gmail.com>
2019-06-23 19:06:39 +0200

    Add navigation mobile

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const navigation = $(".navigation");
+let isMobileMenuOpen = false;
 
+function toggleMobileMenu() {
+  isMobileMenuOpen = !isMobileMenuOpen;
+  $("#nav-mobile").fadeToggle();
+  $(".navigation-toggle").toggleClass("open");
+  $("html").toggleClass("disable-scroll");
+}
+
+// Change navigation class if user scrolls down (make it brighter)
 $(window).scroll(function() {
   if ($(window).scrollTop() > 1) {
     navigation.addClass("navigation--bright");
@@ -7,3 +16,22 @@ $(window).scroll(function() {
     navigation.removeClass("navigation--bright");
   }
 });
+
+// If user had mobile navigation open, close it on > XS size
+$(window).resize(function() {
+  const windowWidth = $(window).width();
+
+  if (windowWidth > 576 && isMobileMenuOpen) {
+    toggleMobileMenu();
+  }
+});
+
+$(".navigation-toggle").on("click", function() {
+  toggleMobileMenu();
+});
+
+$("#mu-about-link, #mu-skills-link, #mu-projects-link, #mu-contact-link").click(
+  function() {
+    toggleMobileMenu();
+  }
+);
